b697eb007afddf2e24da95faa4c81d5270cb7eed
offchain-audit <jy@offchainsec.com>
2024-12-09 16:18:50 -0500

    4. Token Balances and tests

--- test/Token.js
+++ test/Token.js
@@ -5,11 +5,14 @@ const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(),"ether")
 }
 describe("Token", ()=> {
-    let token
+    let token, accounts, deployer
 
     beforeEach(async () => {
         const Token = await ethers.getContractFactory("Token")
         token = await Token.deploy("Socrates", "SOCR", "1000000")
+
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
     })
 
     describe("Deployment", () => {
@@ -33,6 +36,10 @@ describe("Token", ()=> {
     
         it("Has correct total supply.", async ()=> {
             expect(await token.totalSupply()).to.equal(tokens(totalSupply))
+        })
+
+        it("Assigns total supply to deployer.", async ()=> {
+            expect(await token.balanceOf(deployer.address)).to.equal(tokens(totalSupply))
         })
     }) 
 })
