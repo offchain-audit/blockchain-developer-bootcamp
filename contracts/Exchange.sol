5d0d9b9edc3de53b554c4e6eb7ff4b06dcc448df
offchain-audit <jy@offchainsec.com>
2024-12-29 10:15:46 -0500

    8. Create exchange, tests.

---
+++ contracts/Exchange.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Exchange {
+    address public feeAccount;
+    uint256 public feePercent;
+
+    constructor(address _feeAccount, uint256 _feePercent) {
+        feeAccount = _feeAccount;
+        feePercent = _feePercent;
+    }
+
+
+}
+
+    // deposit tokens
+
+    // withdraw tokens
+
+    // check balances
+    
+    // make orders
+
+    // cancel orders
+
+    // fill orders
+
+    // charge fees
+
+    // track fee account
