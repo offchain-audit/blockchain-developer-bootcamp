e7aa5c7df6d8ee6a0d4de55f00fb4d61895d6b93
offchain-audit <jy@offchainsec.com>
2025-01-03 15:45:01 -0500

    11. Make orders, tests.

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -4,23 +4,53 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "./Token.sol";
 
+
 contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order ) public orders;
+    uint256 public orderCount;
 
-    event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Deposit(
+        address token, 
+        address user, 
+        uint256 amount, 
+        uint256 balance
+        );
+
     event Withdraw(
         address token,
         address user,
         uint256 amount,
         uint256 balance
     );
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+    struct _Order {
+        // Order Attributes
+        uint256 id;
+        address user;
+        address tokenGet;
+        uint256 amountGet;
+        address tokenGive;
+        uint256 amountGive;
+        uint256 timestamp;
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
+    
     // -----------------------
     // Deposit & Withdraw Tokens
 
@@ -52,18 +82,44 @@ contract Exchange {
     {
         return tokens[_token][_user];
     }
+
+    // -----------------------------
+    // MAKE & CANCEL ORDERS
+
+    // Token Give (user spend token)
+    // Token Get (user wants token)
+    function makeOrder(
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+        ) 
+        public {
+
+            // PRE-ORDER TOKEN BALANCE CHECK
+            require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+
+            // INSTANTIATE NEW ORDER
+            orderCount = orderCount + 1;
+            orders[orderCount] = _Order(
+                orderCount,
+                msg.sender,
+                _tokenGet,
+                _amountGet,
+                _tokenGive,
+                _amountGive,
+                block.timestamp
+            );
+
+            // EMIT EVENT
+            emit Order(
+                orderCount,
+                msg.sender,
+                _tokenGet,
+                _amountGet,
+                _tokenGive,
+                _amountGive,
+                block.timestamp
+            );
+    }
 }
-
-
-
-    // withdraw tokens
-
-    // check balances
-    
-    // make orders
-
-    // cancel orders
-
-    // fill orders
-
-    // charge fees
