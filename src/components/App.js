69c2c8dfb6916dac36e1efba6ba13f5a13e6a012
offchain-audit <jy@offchainsec.com>
2025-02-06 15:44:13 -0500

    15. UI setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,39 @@
 import { useEffect } from 'react';
 import { ethers } from 'ethers';
 import TOKEN_ABI from '../abis/Token.json';
-import '../App.css';
 import config from '../config.json';
 import { AbiCoder, isAddress } from 'ethers/lib/utils';
-
-
+import store from '../store/store';
+import { useSelector, useDispatch } from 'react-redux';
+import Web3 from 'web3';
 
 function App() {
 
+  const dispatch = useDispatch()
+
   const loadBlockchainData = async () => {
+    try {
+      if (typeof window.ethereum !== 'undefined') {
+          const web3 = new Web3(window.ethereum);
+
+          // Request account access
+          await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+          // Get accounts
+          const accounts = await web3.eth.getAccounts();
+          console.log('Connected accounts:', accounts);
+      } else {
+          // Notify the user to install MetaMask
+          alert('MetaMask is not installed. Please install it to use this DApp: https://metamask.io/');
+      }
+  } catch (error) {
+      console.error('Error loading blockchain data:', error);
+  }    
     const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
     console.log(accounts[0])
 
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    dispatch({ type: 'PROVIDER_LOADED', connection: provider })
     const { chainId } = await provider.getNetwork()
     console.log(chainId)
 
